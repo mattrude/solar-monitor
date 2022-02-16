5be11c0c7e69979a6523910c27fa276da09b5cdc
Matt Rude <matt@mattrude.com>
2022-02-15 21:07:06 -0600

    correct name

--- MPLAB.X/nbproject/Makefile-variables.mk
+++ MPLAB.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=PIC16F15214-solar-monitor.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/PIC16F15214-solar-monitor.X.production.hex
+CND_ARTIFACT_NAME_default=MPLAB.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MPLAB.X.production.hex
