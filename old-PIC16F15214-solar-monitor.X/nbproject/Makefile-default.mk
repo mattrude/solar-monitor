222dab0da347d05d75852f1853bc44c33f3e8965
Matt Rude <matt@mattrude.com>
2022-02-15 20:53:45 -0600

    Correct c code

--- old-PIC16F15214-solar-monitor.X/nbproject/Makefile-default.mk
+++ old-PIC16F15214-solar-monitor.X/nbproject/Makefile-default.mk
@@ -152,11 +152,3 @@ endif
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${OBJECTDIR}
 	${RM} -r ${DISTDIR}
-
-# Enable dependency checking
-.dep.inc: .depcheck-impl
-
-DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
-ifneq (${DEPFILES},)
-include ${DEPFILES}
-endif
