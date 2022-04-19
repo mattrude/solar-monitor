a643de5fcdb657c31deeebc40c6b3d534c34249f
Matt Rude <matt@mattrude.com>
2022-04-18 22:31:08 -0500

    Update main.c

--- SourceCode/AtmelStudio/main.c
+++ SourceCode/AtmelStudio/main.c
@@ -2,17 +2,13 @@
  * main.c
  *
  * Created: 4/18/2022 10:19:17 PM
- *  Author: Owner
+ *  Author: Matt Rude <matt@mattrude.com>
  */ 
 
 #include <xc.h>
 
-
-
-int main(void)
-{
-    while(1)
-    {
+int main(void) {
+    while(1) {
         //TODO:: Please write your application code 
     }
 }
