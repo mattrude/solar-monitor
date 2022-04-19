b306cb82d88268d6bc140a06387dabb76a94c305
Matt Rude <matt@mattrude.com>
2022-04-18 22:28:30 -0500

    Move to Atmel Studio

---
+++ SourceCode/AtmelStudio/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ * Created: 4/18/2022 10:19:17 PM
+ *  Author: Owner
+ */ 
+
+#include <xc.h>
+
+
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
